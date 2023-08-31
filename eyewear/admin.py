d304a7792c178e0bbe592b9c92d2dc5f940b5313
petkovikjandrej <petkovikjandrej@gmail.com>
2023-08-31 03:25:35 +0200

    Registered the missing models (Order and OrderItem)

--- eyewear/admin.py
+++ eyewear/admin.py
@@ -1,9 +1,11 @@
 # admin.py
 
 from django.contrib import admin
-from .models import Product, Cart, CartItem, Color
+from .models import Product, Cart, CartItem, Color, Order, OrderItem
 
 admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(CartItem)
 admin.site.register(Color)
+admin.site.register(Order)
+admin.site.register(OrderItem)
